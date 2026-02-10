0b413ad135e6a40990dbf6e1cdeebcac1e038301
Felix <felixcampidell@gmail.com>
2026-02-10 11:40:48 +0100

    tested vamm on test-data

--- code/main.py
+++ code/main.py
@@ -1,7 +1,46 @@
+import numpy as np
+from itertools import product
+
 from visualization import visualize
+from data_generation.basic_manifolds import line_in_2d, circle, swiss_roll
+from vamm import Gaussian
+
+
+
+def run(cov_type, shared, data, num_components, manifold_dimension, seed):
+    N, D = data.shape
+    rng = np.random.default_rng(seed)
+
+    model = Gaussian(
+        C=num_components,
+        D=D,
+        H=manifold_dimension,  # H is only needed for "mfa"
+        covariance_type=cov_type,
+        shared=shared,
+    )
+    _shared = "with shared covariance matrices" if shared else ""
+    print(f"Train {cov_type} {_shared} ...")
+    obj, logs = model.fit(
+        X=data,
+        rng=rng,
+    )
+    print(f"Objective = {obj:<10.5f}\n")
+
+
 
 
 def main():
+    cov_types = ["isotropic", "diagonal", "mfa", "full"]
+    shared_list = [True, False]
+
+    C = 15 # gaussian components
+    H = 1 # internal manifold-dimension
+    seed = 123
+    rng = np.random.default_rng(seed)
+
+    n = 200 # number of data points
+    data = line_in_2d(n)
+
     print("Select dataset to visualize:")
     print("1: 1D curve in 2D")
     print("2: Circle")
@@ -9,13 +48,22 @@ def main():
     choice = input("Enter choice (1/2/3): ")
 
     if choice == "1":
-        visualize.plot_line_in_2d()
+        data = line_in_2d(n)
     elif choice == "2":
-        visualize.plot_circle()
+        data = circle(n)
     elif choice == "3":
-        visualize.plot_swiss_roll()
+        n = 800
+        H = 2 # this manifold is 2-dimensional. (TODO: why is the objective lower, when using H=1?)
+        data = swiss_roll(n)
     else:
         print("Invalid choice")
+    
+    # train the gaussian model
+    for cov_type, shared in product(cov_types, shared_list):
+        obj = run(cov_type, shared, data, C, H, seed)
+
+    # visualize the data; TODO: visualize the resulting gaussian-components
+    visualize.visualize_data(data)
 
 
 if __name__ == "__main__":
--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -2,28 +2,17 @@ import matplotlib.pyplot as plt
 from data_generation.basic_manifolds import line_in_2d, circle, swiss_roll
 
 
-def plot_line_in_2d(n=500):
-    data = line_in_2d(n)
-    plt.figure()
-    plt.scatter(data[:, 0], data[:, 1], s=5)
-    plt.title("1D curve in 2D")
-    plt.axis("equal")
-    plt.show()
-
-
-def plot_circle(n=500):
-    data = circle(n)
-    plt.figure()
-    plt.scatter(data[:, 0], data[:, 1], s=5)
-    plt.title("Circle")
-    plt.axis("equal")
-    plt.show()
-
-
-def plot_swiss_roll(n=1000):
-    data = swiss_roll(n)
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(data[:, 0], data[:, 1], data[:, 2], s=5)
-    ax.set_title("Swiss Roll")
-    plt.show()
+def visualize_data(data):
+    N, D = data.shape
+    if D == 2:
+        plt.figure()
+        plt.scatter(data[:, 0], data[:, 1], s=5)
+        plt.axis("equal")
+        plt.show()
+    elif D == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], s=5)
+        plt.show()
+    else:
+        print("visualisation of dimension higher than 3 is not supported yet.")
